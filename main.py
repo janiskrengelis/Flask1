1da03b1699c2a992c9c520ba40358a584e94cd4a
janiskrengelis <janis0064@gmail.com>
2020-01-14 10:00:17 +0000

    Changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import *
 app=Flask(__name__)
-@app.route('/',methods=["GET"])
+@app.route('/',)
 
 def getIndex():
   return "<a href='/about'><h1>About</h1></a> <a href='/contact'><h1>Contacts</h1></a> <a href='/home'><h1>Home</h1></a> <p>teksts teksts teksts teksts teksts teksts teksts teksts teksts </p>"
@@ -17,4 +17,5 @@ def getHome():
 def getContact():
   return render_template('contact.html', phone= 12341234)
 
-app.run(host="0.0.0.0",port=8020)
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", threaded=True, port=5000, debug=True)
